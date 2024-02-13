5ef9f1caf9455a3b8d120320262e50ee9c0cfa00
Bestfold <143211991+Bestfold@users.noreply.github.com>
2024-02-13 11:30:59 +0100

    Initial commit

---
+++ src/main/java/com/example/data1500oblig1v24/Data1500Oblig1v24Application.java
@@ -0,0 +1,13 @@
+package com.example.data1500oblig1v24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1500Oblig1v24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1500Oblig1v24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data1500oblig1v24/Data1500Oblig1v24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1500oblig1v24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1500Oblig1v24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
